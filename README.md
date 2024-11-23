6f13751e713bae77ca2abeb5221069b781dedb0d
Xinxia <985025074@qq.com>
2024-11-23 20:06:30 +0800

    fix bugs in eslint config

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ## Update:
 ### 17点12分 2024年11月21日
 use eslint.config.js .instead of .estrinc!
+进一步研究：TODO
+ref:
+https://stackoverflow.com/questions/49789177/module-is-not-defined-and-process-is-not-defined-in-eslint-in-visual-studio-code/78431284#78431284
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,48 @@
 import globals from "globals";
+import js from "@eslint/js";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-
+import pluginJest from "eslint-plugin-jest";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  js.configs.recommended,
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    ignores: ["**/*.test.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.cjs"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.amd,
+      },
+    },
+  },
+  {
+    // update this to match your test files
+    files: ['**/*.spec.js', '**/*.test.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
 ];
